b54321227c8dbb40bfaffc873ed8ef9538aa2784
skullfear <141983125+skullfear@users.noreply.github.com>
2023-09-20 07:43:31 -0500

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,12 @@
+from Docente import *
+from Estudiante import *
+
+class biblioteca:
+    def __init__ (self, archivos_libros, libros, cargar_libros):
+        self.libros= libros
+        self.cargar_libros= archivos_libros
+
+input ("ingrese el nombre del estudiante:")
+if 
+input ("ingrese la carrera del estudiante:")
+
---
+++ libros.py
@@ -0,0 +1,6 @@
+print("holi")
+with open("libros.txt") as archivo:
+    for i in range(10):
+        nombre = str 
+        archivo.write(nombre: {str} - Carácter: {caracter}\n")
+print("Archivo 'caracteres_ascii.txt' creado exitosamente.")
