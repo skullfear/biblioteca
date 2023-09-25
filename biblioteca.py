e1f9f9e029deb791ceb55c1e7a166b4435813b6e
skullfear <141983125+skullfear@users.noreply.github.com>
2023-09-25 06:52:07 -0500

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,12 +1,9 @@
-from Docente import *
-from Estudiante import *
+class libro :
+    
+       
+    def prestado(self):
+        pass
 
-class biblioteca:
-    def __init__ (self, archivos_libros, libros, cargar_libros):
-        self.libros= libros
-        self.cargar_libros= archivos_libros
-
-input ("ingrese el nombre del estudiante:")
-if 
-input ("ingrese la carrera del estudiante:")
+    def disponible(self):
+        pass
 
